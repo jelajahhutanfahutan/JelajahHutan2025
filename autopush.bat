72601cf355eff31f24c44ed5595d499baea4128b
jelajahhutanfahutan <jelajahhutanfahutan@gmail.com>
2025-11-02 17:43:54 +0700

    auto update

---
+++ autopush.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "C:\Users\LENOVO\Documents\AWEB\JaHu2025"
+git add .
+git commit -m "auto update"
+git push origin main
+echo -------------------------
+echo Sudah auto push ke GitHub!
+pause
